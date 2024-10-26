1ac4c5cc9e719eb6bffb93c122b1fdfc9dfbb8a0
Sarah CJ <sarah4594@gmail.com>
2024-10-26 16:15:29 -0500

    Complete exercise 6.1
    Inline styles

--- exercises/06.styling/01.problem.style/index.tsx
+++ exercises/06.styling/01.problem.style/index.tsx
@@ -1,21 +1,36 @@
 import { createRoot } from 'react-dom/client'
 
-// 🐨 add a className prop to each div and apply the correct class names
-// based on the text content
-// 💰 Here are the available class names: box, box--large, box--medium, box--small
-// 💰 each of the elements should have the "box" className applied
-
-// 🐨 add a style prop to each div so their background color
-// matches what the text says it should be
-// 🐨 also use the style prop to make the font italic
-// 💰 Here are available style attributes: backgroundColor, fontStyle
-
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+const smallBox = (
+	<div
+		className="box box--small"
+		style={{ backgroundColor: 'lightblue', fontStyle: 'italic' }}
+	>
+		small lightblue box
+	</div>
+)
+const mediumBox = (
+	<div
+		className="box box--medium"
+		style={{ backgroundColor: 'pink', fontStyle: 'italic' }}
+	>
+		medium pink box
+	</div>
+)
+const largeBox = (
+	<div
+		className="box box--large"
+		style={{ backgroundColor: 'orange', fontStyle: 'italic' }}
+	>
+		large orange box
+	</div>
+)
 
 // 💰 the sizelssColorlessBox should still be a box, just with no size or color
-const sizelessColorlessBox = <div>sizeless colorless box</div>
+const sizelessColorlessBox = (
+	<div className="box" style={{ fontStyle: 'italic' }}>
+		sizeless colorless box
+	</div>
+)
 
 function App() {
 	return (
