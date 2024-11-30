0a486ed5ac7d83a82121e981fa64683b7fda4645
Sarah Carter-Johnson <sarah4594@gmail.com>
2024-11-30 00:25:13 -0600

    Complete exercise 8.5
    Default values

--- exercises/08.inputs/05.problem.default-value/index.tsx
+++ exercises/08.inputs/05.problem.default-value/index.tsx
@@ -10,7 +10,11 @@ function App() {
 			<div>
 				<label htmlFor="accountTypeSelection">Account Type:</label>
 				{/* 🐨 set the default value to "student" */}
-				<select id="accountTypeSelection" name="accountType">
+				<select
+					id="accountTypeSelection"
+					name="accountType"
+					defaultValue="student"
+				>
 					<option value="">--Please select an option--</option>
 					<option value="admin">Admin</option>
 					<option value="teacher">Teacher</option>
@@ -29,7 +33,14 @@ function App() {
 			<div>
 				<label htmlFor="ageInput">Age:</label>
 				{/* 🐨 set the default value to 18 */}
-				<input id="ageInput" name="age" type="number" min="0" max="200" />
+				<input
+					id="ageInput"
+					name="age"
+					type="number"
+					min="0"
+					max="200"
+					defaultValue="18"
+				/>
 			</div>
 			<div>
 				<label htmlFor="photoInput">Photo:</label>
@@ -38,13 +49,18 @@ function App() {
 			<div>
 				<label htmlFor="colorInput">Favorite Color:</label>
 				{/* 🐨 set the default value to #002E5D */}
-				<input id="colorInput" name="color" type="color" />
+				<input
+					id="colorInput"
+					name="color"
+					type="color"
+					defaultValue="#002E5D"
+				/>
 			</div>
 			<fieldset>
 				<legend>Visibility:</legend>
 				<label>
 					{/* 🐨 set the default value to "Public" */}
-					<input name="visibility" type="radio" value="public" />
+					<input name="visibility" type="radio" value="public" defaultChecked />
 					Public
 				</label>
 				<label>
@@ -55,14 +71,20 @@ function App() {
 			<div>
 				<label>
 					{/* 🐨 set the default value to checked */}
-					<input name="waiver" type="checkbox" />
+					<input name="waiver" type="checkbox" defaultChecked />
 					Waiver Signed
 				</label>
 			</div>
 			<div>
 				{/* 🐨 set the default value to today */}
 				<label htmlFor="startDateInput">Start Date:</label>
-				<input id="startDateInput" name="startDate" type="date" />
+				<input
+					id="startDateInput"
+					name="startDate"
+					type="date"
+					// defaultValue="2024-11-30"
+					defaultValue={new Date().toISOString().slice(0, 10)}
+				/>
 			</div>
 			<button type="submit">Submit</button>
 		</form>
