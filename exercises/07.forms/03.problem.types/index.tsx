5c88852dfcdc741ca6d6edee0fd034c78c452bcd
Sarah Carter-Johnson <sarah4594@gmail.com>
2024-11-29 23:48:31 -0600

    Complete exercise 7.3
    Form input types

--- exercises/07.forms/03.problem.types/index.tsx
+++ exercises/07.forms/03.problem.types/index.tsx
@@ -2,10 +2,32 @@ import { createRoot } from 'react-dom/client'
 
 function App() {
 	return (
-		<form action="api/onboarding">
+		<form action="api/onboarding" method="POST">
 			<div>
-				<label htmlFor="usernameInput">Username:</label>
-				<input id="usernameInput" name="username" />
+				<div>
+					<label htmlFor="usernameInput">Username:</label>
+					<input id="usernameInput" name="username" />
+				</div>
+				<div>
+					<label htmlFor="passwordInput">Password:</label>
+					<input name="password" type="password" id="passwordInput" />
+				</div>
+				<div>
+					<label htmlFor="ageInput">Age:</label>
+					<input name="age" type="number" min="0" max="200" id="ageInput" />
+				</div>
+				<div>
+					<label htmlFor="photoInput">Photo:</label>
+					<input name="photo" type="file" accept="image/*" id="photoInput" />
+				</div>
+				<div>
+					<label htmlFor="colorInput">Favorite Color:</label>
+					<input name="color" type="color" id="colorInput" />
+				</div>
+				<div>
+					<label htmlFor="dateInput">Start Date:</label>
+					<input name="date" type="date" id="dateInput" />
+				</div>
 			</div>
 			{/* 🐨 add appropriate inputs for:
 				- password
